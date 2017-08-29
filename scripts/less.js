01c00564c14cfb45b4c8a6b9e94e34cf4fdd7be3
John Chleborad <jchleborad@gmail.com>
2017-08-28 21:14:44 -0700

    car inv filter

--- scripts/less.js
+++ scripts/less.js
@@ -1,25 +1,47 @@
-(function($) {
-  jQuery.expr[':'].Contains = function(a, i, m) {
-    return (a.textContent || a.innerText|| "").toUpperCase().indexOf(m[3].toUpperCase())>= 0;
-  };
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchBox');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("carThumb");
+    li = ul.getElementsByTagName('li');
 
-  function listFilter(header, list) {
-    $('#searchbox')
-      .change(function() {
-        var filter = $(this).val();
-        if (filter) {
-          $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
-          $(list).find("a:Contains(" + filter + ")").parent().slideDown();
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
         } else {
-          $(list).find("li").slideDown();
+            li[i].style.display = "none";
         }
-        return false;
-      })
-      .keyup(function() {
-        $(this).change();
-      });
-  }
-  $(function() {
-    listFilter($(), $("#list"));
-  });
-}(jQuery));
+    }
+}
+
+
+
+
+// (function($) {
+//   jQuery.expr[':'].Contains = function(a, i, m) {
+//     return (a.textContent || a.innerText|| "").toUpperCase().indexOf(m[3].toUpperCase())>= 0;
+//   };
+//
+//   function listFilter(header, list) {
+//     $('#searchbox')
+//       .change(function() {
+//         var filter = $(this).val();
+//         if (filter) {
+//           $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
+//           $(list).find("a:Contains(" + filter + ")").parent().slideDown();
+//         } else {
+//           $(list).find("li").slideDown();
+//         }
+//         return false;
+//       })
+//       .keyup(function() {
+//         $(this).change();
+//       });
+//   }
+//   $(function() {
+//     listFilter($(), $("#list"));
+//   });
+// }(jQuery));
